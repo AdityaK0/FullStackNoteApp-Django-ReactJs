bbbdf9122368cea9a078171175bde95821de6013
AdityaK0 <aditya123905@gmail.com>
2025-03-25 16:53:34 +0530

    setting user module above all modules due to customuser model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',   # above admin because of custom model rememeber that(yaad rakhiyo)
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     'notes',
     'corsheaders',
     'rest_framework',
-    'users',
     'rest_framework_simplejwt.token_blacklist',
 ]
 
